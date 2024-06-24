f33101c6dc9700fe1d291057120890efcdc5d89b
Neto <j.altevirneto@gmail.com>
2024-06-24 10:45:29 -0300

    feat: add conta moda

--- exercicio.js
+++ exercicio.js
@@ -1,27 +1,55 @@
-let array = [];
+let numeros = [];
 let num;
+let repeticao = {};
 
 console.log("Digite um numero para fazer sua média, mediana e moda ");
 process.stdin.on("data", function(data){
 num = data.toString().trim();
 if(num == 'finalizar' || num == 'f'){
+//-------------------------------------------------
+//processo média
     let soma = 0;
     let media = 0;
-    for(let i = 0; i < array.length; i++){
-        soma = soma + array[i];
+    for(let i = 0; i < numeros.length; i++){
+        soma = soma + numeros[i];
     }
-    media = soma / array.length;
-
-    console.log("A média é " + media);
-    process.exit();
-
-}else{
-    if(!isNaN(num)){
-    array.push(+num);
-    console.log("Número adicionado com sucesso");
-    console.log("Insira outro numero ou calcule a média, mediana e moda escrevendo finalizar");
-    }else{
-        console.log("Digite um número válido");
+    media = soma / numeros.length;
+//--------------------------------------------------------------
+//processo moda
+for (let i = 0; i < numeros.length; i++) {
+    if(repeticao[numeros[i]]) {
+        repeticao[numeros[i]]++
+    } else {
+        repeticao[numeros[i]] = 1
     }
 }
+
+let array = Object.entries(repeticao)
+
+let moda
+let quantidadeAparicoes = 0
+
+for(let i = 0; i < array.length; i++) {
+    if(array[i][1] > quantidadeAparicoes) {
+        quantidadeAparicoes = array[i][1]
+        moda = array[i][0]
+    }
+}
+
+
+//-------------------------------------------------------------- 
+    console.log("A média é: ", media);
+    console.log("A moda é:", moda);
+    console.log("")
+    process.exit();
+
+} else {
+    if(!isNaN(num)){
+    numeros.push(+num);
+    console.log("Número adicionado com sucesso");
+    console.log("Insira outro numero ou calcule a média, mediana e moda escrevendo finalizar");
+    }else {
+        console.log("Digite um número válido");
+    }
+    }
 });
